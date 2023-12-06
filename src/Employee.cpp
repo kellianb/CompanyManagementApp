98e7d7114ae92d0dec1bb1a149969326985236fb
Kellian B <109019244+dercrafter@users.noreply.github.com>
2023-12-06 14:17:55 +0100

    - Added employee class

---
+++ src/Employee.cpp
@@ -0,0 +1,59 @@
+﻿#include "Employee.h"
+
+void Employee::create_employee_in_DB()
+{
+}
+
+void Employee::fetch_employee_from_DB()
+{
+}
+
+void Employee::modify_employee_in_DB()
+{
+}
+
+void Employee::delete_employee_from_DB()
+{
+}
+
+Employee::Employee(int id) : Person(id)
+{
+    this->fetch_employee_from_DB();
+}
+
+Employee::Employee(System::String^ first_name, System::String^ last_name, System::DateTime hire_date, int id_adress) : Person(first_name, last_name)
+{
+    this->create_employee_in_DB();
+}
+
+int Employee::getIDaddress()
+{
+    return this->id_address;
+}
+
+void Employee::setIDaddress(int id)
+{
+    this->id_address = id;
+}
+
+System::DateTime Employee::getHireDate()
+{
+    return this->hire_date;
+}
+
+void Employee::setHireDate(System::DateTime date)
+{
+    this->hire_date = date;
+}
+
+void Employee::modify_in_DB()
+{
+    this->modify_person_in_DB();
+    this->modify_employee_in_DB();
+}
+
+void Employee::delete_in_DB()
+{
+    this->delete_employee_from_DB();
+    this->delete_person_from_DB();
+}
---
+++ src/Employee.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Customer.h"
+
+ref class Employee : Person
+{
+private:
+    int id_address;
+    System::DateTime hire_date;
+    
+    // Private DB modification methods
+    void create_employee_in_DB();
+    void fetch_employee_from_DB();
+
+    // These methods only affect the "Customers" Table, not the parent "People" table
+    void modify_employee_in_DB();
+    void delete_employee_from_DB();
+
+public:
+    Employee(int);
+    Employee(System::String^, System::String^, System::DateTime, int);
+
+    int getIDaddress();
+    void setIDaddress(int);
+    
+    System::DateTime getHireDate();
+    void setHireDate(System::DateTime);
+
+    void modify_in_DB() override;
+    void delete_in_DB() override;
+};
